804a205ad0d8970dcc63b50e3edcc7819092e011
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-10-22 05:17:37 -0600

    ⁠El Consejo de los Tres Maestros

--- index.js
+++ index.js
@@ -1,40 +1,20 @@
-/* Ejercicio: Contar números positivos y negativos
-Descripción: Escribe un programa que reciba una lista de números enteros 
-y cuente cuántos de ellos son positivos y cuántos son negativos. Al final,
-el programa debe mostrar ambos conteos.
+/*  ⁠El Consejo de los Tres Maestros
+Contexto temático:
+Tres Maestros Jedi están decidiendo quién será el líder de la próxima misión diplomática,
+ y tu tarea es ayudar a determinar quién tiene más experiencia.
+
+Descripción:
+Dado tres números que representan los años de experiencia de cada Maestro Jedi, 
+encuentra cuál es el mayor.
+
 Instrucciones:
-Pide al usuario que ingrese una lista de números separados por comas.
-Usa un ciclo for para recorrer la lista de números.
-Usa la estructura condicional if para verificar si cada número es positivo o negativo.
-Lleva la cuenta de cuántos números positivos y negativos hay.
-Al final, imprime el resultado.
+Escribe una función que reciba tres números enteros y devuelva el mayor de ellos.
+
 Pista:
-Un número es positivo si es mayor que 0 y es negativo si es menor que 0.
- */
+Utiliza las comparaciones if para determinar cuál de los tres números es el mayor. */
 
-let numeros = prompt("ingresa 10 numeros positivos y negativos, separados por comas");
-let conversionDeNumeros = numeros.split(",").map(numeros => parseInt(numeros.trim()));
-let listaNumeros = conversionDeNumeros
-/* let listaNumeros = [3, -1, 5, -2, 7, -6, 0, 8];  */
+let edades = (prompt("Cuales son sus edades?"));
+let edad = edades.split(',').map(Number);
 
-let numPositivos = []
-let numNegativos = []
-
-for (let i = 0; i < listaNumeros.length; i++) {
-    if (listaNumeros[i] > 0) {
-        numPositivos.push(listaNumeros[i])
-    } else if (listaNumeros[i] < 0){
-        numNegativos.push(listaNumeros[i]) 
-    }
-}
-
-console.log("Lista de numeros Negativos: "+ numNegativos);
-console.log("Lista de numeros Positivos: "+ numPositivos);
-
-
-/*    let num = [ -9, -12, -4, -6, -2]
-
-   for(let i = 0; i < num ; i++ ){
-    console.log(i);
-    
-   } */
+let maxEdad = Math.max(...edad)
+console.log("El es le mayor : " + maxEdad);
