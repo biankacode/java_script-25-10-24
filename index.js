4dde0c1cd8cbddbd4d443ef570b134cfe004e7a7
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-10-22 05:37:47 -0600

    Loop: for of

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
-/*  ⁠El Consejo de los Tres Maestros
-Contexto temático:
-Tres Maestros Jedi están decidiendo quién será el líder de la próxima misión diplomática,
- y tu tarea es ayudar a determinar quién tiene más experiencia.
+let canasta = ["manzana", "pera", "platano", "naranja"]
 
-Descripción:
-Dado tres números que representan los años de experiencia de cada Maestro Jedi, 
-encuentra cuál es el mayor.
-
-Instrucciones:
-Escribe una función que reciba tres números enteros y devuelva el mayor de ellos.
-
-Pista:
-Utiliza las comparaciones if para determinar cuál de los tres números es el mayor. */
-
-let edades = (prompt("Cuales son sus edades?"));
-let edad = edades.split(',').map(Number);
-
-let maxEdad = Math.max(...edad)
-console.log("El es le mayor : " + maxEdad);
+for (fruta of canasta){
+    console.log(fruta);
+}
