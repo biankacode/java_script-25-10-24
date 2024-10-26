ab7de361581799b156f2e812888bdeaa8ca54d2e
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-10-26 07:04:36 -0600

    Sumar los primeros 100 números

---
+++ 3_suma_del_1_100.js
@@ -0,0 +1,9 @@
+/* Ejercicio 3: Sumar los primeros 100 números
+Este ejercicio te ayudará a ver cómo un ciclo puede ser usado para realizar cálculos.
+Objetivo: Usa un ciclo for para sumar todos los números del 1 al 100 y mostrar el resultado final. */
+let suma = 0
+
+for (let i = 1; i <= 100; i++ ){
+   suma = suma + i; /* toma el valor actual de suma y agrégale el valor de i */
+}
+console.log("la suma de los primeros 100 numeros es : " + suma );
