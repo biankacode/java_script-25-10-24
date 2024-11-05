dbad3fcac0622a8be25526def82007c7c5d8f03f
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-11-05 05:39:53 -0600

    multiplicar

---
+++ 6_multiplicar.js
@@ -0,0 +1,17 @@
+/* 3. Multiplicar por 2
+Usa el ciclo para mostrar los resultados de multiplicar el número actual por 2, cada vez:
+
+Empieza desde 1 hasta 5.
+Multiplica el número por 2 y muestra el resultado.
+Esto ayuda a ver cómo el valor de la variable cambia en cada iteración, como si fueran pequeños pasos. */
+
+function multiplicar (num){  
+
+    for (let i = 1; i <= 11; i++){
+        console.log(i * num);
+    }
+    return num
+}
+multiplicar(3)
+ 
+
