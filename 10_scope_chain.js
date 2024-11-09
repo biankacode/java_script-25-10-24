c4602ddfc4e54c8d45858c56e92dec03fed6d14b
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-11-09 12:42:26 -0600

    contextos de ejecucion y scope chain

---
+++ 10_scope_chain.js
@@ -0,0 +1,14 @@
+/* Contextos de ejecución y scope chain */
+
+const  prdoductName = "Smarpone"
+const price = 499
+const brand = "TechCo"
+
+function getProductoDetalist() {
+    const prdoductName = "Laptop"
+    const price = 899 
+
+    return `The prduct ${prdoductName} cost  $${price} and is o the brand ${brand}` 
+}
+ 
+console.log(`The prduct ${prdoductName} cost  $${price} and is o the brand ${brand}`);
