29160e526e19db6228d078084c6a8f05901ab15d
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-11-21 05:42:50 -0600

    13 suma dos numeros

---
+++ 13.1suma_dos_numeros.js
@@ -0,0 +1,8 @@
+/* 1. Suma de dos números: Escribe una función que reciba dos números como argumentos y devuelva su suma. */
+
+function suma(num1, num2) {
+    let resultado = num1 + num2
+    return resultado
+} 
+console.log(suma(23,34));
+
