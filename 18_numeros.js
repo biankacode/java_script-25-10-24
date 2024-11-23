e1f1dedd8958693ec1b78b30ace11b30cde22bc4
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-11-23 15:32:29 -0600

    18 contar acendente decendente

---
+++ 18_numeros.js
@@ -0,0 +1,23 @@
+
+/* 1. Imprimir los números del 1 al 10
+Escribe un programa que imprima los números del 1 al 10 utilizando un bucle for. */
+function acender(num) {
+    for (let i = 0; i <= num; i++) {
+        let residuo = i
+        console.log("i" ,residuo);
+        
+       }
+}
+acender(10)
+
+function decender(num) {
+    for(let j = num ; j >= 0; j --){
+        let resultado = j
+        console.log(resultado, "j");
+    }
+}
+decender(10)
+
+
+
+
