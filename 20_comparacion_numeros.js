ae2e428f74f80610a6359c440d424b20c648a4e3
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-11-27 05:15:07 -0600

    20 comparacion de numeros

---
+++ 20_comparacion_numeros.js
@@ -0,0 +1,19 @@
+let residuo 
+function comparaNumeros(num0, num1) {
+    if (num0 > num1){
+        residuo = console.log(num0,"es mayor que",num1);
+
+    }else if(num0 === num1){
+        residuo = console.log("son iguales");
+    }
+    else {
+       residuo = console.log(num0, "es menor  que ", num1);
+    }
+    return 
+}
+comparaNumeros(40,229)
+/* 
+for (let i = 0; i ; i++) {
+    const element = array[i];
+    
+}  */
