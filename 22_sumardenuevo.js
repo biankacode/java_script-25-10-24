485877e35622e2a32a2dc4a16bea2ebad8354069
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-12-04 06:18:28 -0600

    22 sumar

---
+++ 22_sumardenuevo.js
@@ -0,0 +1,18 @@
+/* funcion suma */
+
+for (let i = 0; i <= 10; i++){
+    let residuo = i
+    console.log(i,residuo);
+}
+
+const array = [1,2,3,4,5,6,7,8,9]
+for (let j = 0; j < array.length; j++) {
+    const element = array[j];
+    console.log(j,element);
+}
+
+for (let x = 10 ; x > 0; x--){
+    let resultado = x 
+    console.log(resultado);
+    
+}
