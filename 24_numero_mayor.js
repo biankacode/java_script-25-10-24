b323101d31ef3ccb8ef81aee68bce87a42979773
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-12-04 06:34:45 -0600

    24 numero mayor

---
+++ 24_numero_mayor.js
@@ -0,0 +1,13 @@
+/* Verifica si un número es mayor que otro. */
+
+function verificarNumerMayor (a,b) {
+    if ( a > b){
+        console.log(a, " es mayor que ", b);
+        
+    }else {
+        console.log(b , "es mayor que ", a);
+        
+    }
+
+}
+verificarNumerMayor(9, 19)
