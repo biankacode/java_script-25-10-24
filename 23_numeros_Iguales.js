34c59819d38cea13b318fdb42017cd894ec8974e
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-12-04 06:26:35 -0600

    23 numeros iguales

---
+++ 23_numeros_Iguales.js
@@ -0,0 +1,10 @@
+/* Comprueba si dos números son iguales. */
+
+function comprobacionDeNumeros(a,b) {
+    if (a === b) {
+        console.log("es el mismo numero");
+    } else if(a !== b){
+        console.log("son diferentes numeros");
+    }
+}
+comprobacionDeNumeros(1,2)
