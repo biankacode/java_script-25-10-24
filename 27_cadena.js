508654ff7f08181e5dcae333c065dba21390efbd
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-12-06 05:04:54 -0600

    27 largo de cadena texto

---
+++ 27_cadena.js
@@ -0,0 +1,11 @@
+/* Comprueba si la longitud de una cadena es mayor que 5. */
+function largoCadena(cadenaTexto) {
+     
+    if (cadenaTexto.length > 5) {
+        console.log("es muy largo el texto");
+    } else {
+        console.log("demacioado corto");
+        
+    }
+}
+largoCadena("baila")
