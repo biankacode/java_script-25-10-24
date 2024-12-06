91414f3757a000f36c875719d48476fa8b0ae384
Bianca Pelcastre <biancapelcastrevarela@gmail.com>
2024-12-05 21:02:20 -0600

    26 comparacion de textos

---
+++ 26_com_texto.js
@@ -0,0 +1,12 @@
+/* Compara si dos cadenas de texto son diferentes. */
+
+let cadena1 = "El perro baila"
+let cadena2 = "El perro baila"
+
+function comparacion(a,b) {
+    if(cadena1 === cadena2){
+        console.log(cadena1," dice lo  ismo que ", cadena2);
+        
+    }
+}
+comparacion(cadena1, cadena2)
